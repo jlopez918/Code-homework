8f8d3dd674f75460e2c7d50dfe29c89eb37fbed3
Julian Acevedo <jlopezvet918@gmail.com>
2024-07-26 01:16:31 -0500

    Agrego Tarea Clase 2

---
+++ Tarea Clase 2/vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Raziel - Saga Soul Reaver</h1>
+      <img
+        src="https://i0.wp.com/www.pcmrace.com/wp-content/uploads/2017/10/raziel_by_dekades8-d9y39ks.png?resize=750%2C400&ssl=1"
+        alt="Foto Raziel"
+      />
+      <p>
+        Raziel es un Espectro Segador de Almas, como tal Raziel debe consumir
+        las almas de los muertos, para devolverlas a la rueda de la vida. El
+        nombre de Raziel proviene del ángel príncipe de los querubines, su
+        nombre significa "Secreto de Dios".
+      </p>
+    </div>
+  );
+}
+
+export default App;
